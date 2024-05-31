5f2bd3361a2df832f205c91d7c98a2cf4b2fb4fe
ali-shah007 <alishah69.ah@gmail.com>
2024-05-31 07:14:12 +0500

    aa

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -55,12 +55,12 @@ function Navbar() {
           <Link to='/'>Store</Link>
         </div>
         <div className='flex  justify-center items-center gap-5 icon-color cursor-pointer'>
-          <MdOutlineSearch  className=' w-6 h-6' />
-          <CiUser  className=' w-6 h-6' />
-          <CiHeart  className=' w-6 h-6' />
+          <MdOutlineSearch  className=' w-7 h-7' />
+          <CiUser  className=' w-7 h-7' />
+          <CiHeart  className=' w-7 h-7' />
           <div className="flex relative">
-  <LuShoppingCart className=' w-6 h-6' />
-  <span className="w-3 h-3 rounded-full bg-blue-500 text-white flex items-center justify-center absolute top-0 right-0 -mt-1 -mr-1 text-[12px]">0</span>
+  <LuShoppingCart className=' w-7 h-7' />
+  <span className="w-5 h-5 rounded-full bg-blue-500 text-white flex items-center justify-center absolute top-0 right-0 -mt-1 -mr-3 text-sm">0</span>
 </div>
 
           
