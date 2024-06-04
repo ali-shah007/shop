a4c9fc4717f836eb01435643c61f5cee52abc682
ali-shah007 <alishah69.ah@gmail.com>
2024-06-05 00:03:32 +0500

    a

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,8 +4,9 @@ function Categories() {
   return (
     <div className='flex w-[98vw] mx-auto gap-1 h-[80vh] overflow-hidden'>
         <div className='flex w-1/2 gap-1' >
-            <div className='flex w-1/2 flex-col gap-1'>
+            <div className='flex w-1/2 flex-col gap-1 '>
                     <img className=' w-full h-1/2 object-cover' src='https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='firstcategroy' />
+                    
                     <img className=' w-full h-1/2 object-cover'src='https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='firstcategroy' />
             </div>
             <div className='flex w-1/2 h-full'>
